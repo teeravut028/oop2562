4b0519d7ff04d03ffe73fae122ed8c721ea4b8e5
papercut <papercut@172.17.61.48>
2019-12-09 12:11:11 +0700

    9/12/2562 v.2

--- src/GraduateStudent.java
+++ src/GraduateStudent.java
@@ -1,6 +1,18 @@
 public class GraduateStudent {
-    public static void main(String[] args) {
-   
-}
+    protected String studyLevel;
+    protected String thesisAdviser;
+     
+    public void oralExamination(){
+        System.out.println("oralExamination");
+    }   
+    public void thesisExamination(){
+        System.out.println("thesisExamination");
+    }
+    public void showdata (){
+        System.out.println("**************************************");
+        System.out.println("studyLevel : "+studyLevel);
+        System.out.println("ThesisAdviser : "+thesisAdviser);
+        System.out.println("**************************************");
+    }
     
 }
